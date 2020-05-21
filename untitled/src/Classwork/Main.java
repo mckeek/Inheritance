90299f27fe8fce86a4852d81cbd74c8b98f51add
mckeek <kendall@2kmckee.net>
2020-05-21 11:03:22 -0400

    commit the new exercises

---
+++ untitled/src/Classwork/Main.java
@@ -0,0 +1,36 @@
+package Classwork;
+
+public class Main {
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+
+    public static void main(String[] args) {
+        int newscore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newscore);
+
+        newscore = calculateScore(500);
+        newscore = calculateScore();
+
+       System.out.println(calcCentimeters.calcFeetAndInchesToCentimeters(5,6));
+       System.out.println(calcCentimeters.calcFeetAndInchesToCentimeters(-5));
+       System.out.println(calcTime.getDurationString(1000, 58));
+       System.out.println(calcTime.getDurationString(100));
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println(" Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println(" Unnamed player scored  0 points");
+        return 0;
+    }
+}
+
+
